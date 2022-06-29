f330d117cf8a515ba93d6c7d07c25e5ffdd710ea
Kristijan Cetina <kristijan.cetina@gmail.com>
2022-06-30 00:34:09 +0200

    more on basic test

--- paying_customer/playwrightTests/basicSignUp.spec.ts
+++ paying_customer/playwrightTests/basicSignUp.spec.ts
@@ -7,5 +7,12 @@ test.beforeEach(async ({ page }) => {
 test.describe('Open app and check basic elements', () => {
     test('should have basic elements on frontpage', async ({ page }) => {
         await expect(page.locator('.navbar-brand > img')).toBeVisible();
+        await expect(page.locator('text=Make your payments easier')).toBeVisible();
+        await expect(page.locator('[href="/login"]')).toBeVisible();
+        await expect(page.locator('[href="/signup"]')).toBeVisible();
+    });
+    test('should sign up a new user', async ({ page }) => {
+        page.locator('text=Login').click();
+        await expect(page).toHaveURL(/.*login/)
     })
 });
